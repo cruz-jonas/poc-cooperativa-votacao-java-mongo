baf25911365ec7c71c65a8dce785731a9999fdf7
jonas <scruzjonas@gmail.com>
2023-08-16 15:49:58 -0300

    add annotation

--- src/main/java/com/example/cooperative/contract/model/request/VoteRequest.java
+++ src/main/java/com/example/cooperative/contract/model/request/VoteRequest.java
@@ -3,10 +3,12 @@ package com.example.cooperative.contract.model.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class VoteRequest {
 
     private String idAgenda;
